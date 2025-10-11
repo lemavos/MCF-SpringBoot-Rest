16607dce74bdf68fb6533164a6c967d75807b4be
lemavos <henriquelsilva911@gmail.com>
2025-10-10 21:07:48 -0300

    first commit

---
+++ src/main/java/com/lemavos/mcf/McfApplication.java
@@ -0,0 +1,13 @@
+package com.lemavos.mcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemavos/mcf/McfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemavos.mcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
