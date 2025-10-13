97a3a4f4ef48f0989e878242bb404aa415dc10b9
lemavos <henriquelsilva911@gmail.com>
2025-10-13 16:25:56 -0300

    Added Pythagoras Teorem

---
+++ api/src/main/java/com/lemavos/mcf/services/mathUtils/pythagorasTheorem/Hypotenuse.java
@@ -0,0 +1,11 @@
+package com.lemavos.mcf.services.mathUtils.pythagorasTheorem;
+
+public class Hypotenuse {
+    public static String calculateHypotenuse(double leg1, double leg2){
+        if (leg1 <= 0 || leg2 <= 0){
+            return "Leg lengths must be positive numbers.";
+        }
+        double hypotenuse = Math.sqrt(Math.pow(leg1, 2) + Math.pow(leg2, 2));
+        return "The length of the hypotenuse is: " + hypotenuse;
+    }
+}
---
+++ api/src/main/java/com/lemavos/mcf/services/mathUtils/pythagorasTheorem/Leg.java
@@ -0,0 +1,14 @@
+package com.lemavos.mcf.services.mathUtils.pythagorasTheorem;
+
+public class Leg {
+    public static String calculateLeg(double hypotenuse, double leg1){
+        if (hypotenuse <= 0 || leg1 <= 0){
+            return "Hypotenuse and leg lengths must be positive numbers.";
+        }
+        if (leg1 >= hypotenuse){
+            return "Leg length must be less than the hypotenuse.";
+        }
+        double leg2 = Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(leg1, 2));
+        return "The length of the other leg is: " + leg2;
+    }
+}
