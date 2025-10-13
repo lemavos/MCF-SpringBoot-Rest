ef336c2daf8465fb608000146b82b2304b445975
lemavos <henriquelsilva911@gmail.com>
2025-10-13 15:54:58 -0300

    Added Bhaskara

---
+++ src/main/java/com/lemavos/mcf/controller/McfController.java
@@ -0,0 +1,13 @@
+package com.lemavos.mcf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/mcf")
+@RequiredArgsConstructor
+public class McfController {
+    
+}
---
+++ src/main/java/com/lemavos/mcf/services/mathUtils/Bhaskara.java
@@ -0,0 +1,22 @@
+package com.lemavos.mcf.services.mathUtils;
+
+public class Bhaskara {
+    public static String calculate(double a, double b, double c){
+        double delta = calculateDelta(a, b, c);
+        if(delta < 0){
+            return "Delta is negative, no real roots.";
+
+        }
+        if (delta == 0){
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            return "One real root: " + root1;};
+        
+        double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+        double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+        return "Two real roots: " + root1 + " and " + root2;
+    }
+
+    private static double calculateDelta(double a, double b, double c){
+        return (b * b) - 4 * a * c;
+    }
+}
