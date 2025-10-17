4f5f45d21b1dc9e26ba45299bd1fbb56469fe65d
lemavos <henriquelsilva911@gmail.com>
2025-10-17 15:57:29 -0300

    Pythagoras Theorem Controller

--- src/main/java/com/lemavos/mcf/controller/MathController.java
+++ src/main/java/com/lemavos/mcf/controller/MathController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lemavos.mcf.services.mathUtils.Bhaskara;
 import com.lemavos.mcf.services.mathUtils.Factorial;
-import com.lemavos.mcf.services.mathUtils.pythagorasTheorem.*;
+import com.lemavos.mcf.services.mathUtils.pythagorasTheorem.Hypotenuse;
+import com.lemavos.mcf.services.mathUtils.pythagorasTheorem.Leg;
 
 @RestController
 @RequestMapping("/math")
@@ -23,7 +24,21 @@ public class MathController {
     }
 
     @GetMapping("/factorial")
-    public ResponseEntity<?> calculateBhaskara(@RequestParam double number) {
+    public ResponseEntity<?> calculateFactorial(@RequestParam double number) {
         return ResponseEntity.ok(Factorial.calculateFactorial(number));
+    }
+
+    // Pythagoras Theorem
+
+    @GetMapping("pythagoras/hypotenuse")
+    public ResponseEntity<?> calculateHypotenuse(@RequestParam double leg1,
+                                                @RequestParam double leg2) {
+        return ResponseEntity.ok(Hypotenuse.calculateHypotenuse(leg1, leg2));
+    }
+
+    @GetMapping("pythagoras/leg")
+    public ResponseEntity<?> calculateLeg(@RequestParam double hypotenuse,
+                                        @RequestParam double leg1) {
+        return ResponseEntity.ok(Leg.calculateLeg(hypotenuse, leg1));
     }
 }
